52f61ffc00845b2bb688bab8cfc68c57be85aaf9
HuuSang15 <phamhuusang24@gmail.com>
2023-05-19 18:10:05 +0700

    Upload About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import AboutPage from '../components/AboutPage/AboutPage';
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <AboutPage />
+    </div>
   )
 }
 
