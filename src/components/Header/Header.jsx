a27612d78db2b5c22db4594686b9ceb37527c2ef
tanlee <lexuantan159@gmail.com>
2023-05-21 17:05:02 +0700

    finishing header and footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,46 +1,108 @@
-import { faBagShopping } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <div>
-      <header className='container mx-auto max-w-5xl'>
-        <div className='grid grid-flow-col'>
-          <div className="py-6">
-            <h1 className= "" >logo</h1>
-          </div>
-          <ul className='flex py-6 px-3'>
-            <li >
-              <a className='pl-14 text-lg font-bold ' href="/">
-                Home
-              </a>
-            </li>
-            <li>
-              <a className='pl-14 text-lg font-bold ' href="/products">
-                  Our Products
-              </a>
-            </li>
-            <li>
-              <a className='pl-14 text-lg font-bold ' href="/about">
-                About
-              </a>
-            </li>
-            <li>
-              <a className='pl-14 text-lg font-bold ' href="/contact">
-                Contact
-              </a>
-            </li>
-          </ul>
+    const [isOpen, setIsOpen] = useState(false);
+    const router = useLocation();
+    console.log(router.pathname);
 
-          <div className='py-6'>
-          <FontAwesomeIcon icon={faBagShopping} />
-          </div>
+    return (
+        <div>
+            <header className="mx-auto max-w-5xl  h-[76px] flex justify-between sm:px-3 md:px-10 px-[10%] lg:px-0 lg:grid lg:grid-flow-col">
+                <div className="flex items-center">
+                    <h1 className="text-2xl font-bold text-primaryColor">GreenEco</h1>
+                </div>
+
+                <ul className=" px-3 lg:flex lg:items-center ">
+                    <li>
+                        <a
+                            className={`pr-14 text-lg font-bold hover:text-primaryColor transition duration-300 linear ${
+                                router.pathname === '/' ? 'text-primaryColor' : 'text-black'
+                            }`}
+                            href="/"
+                        >
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className={`pr-14 text-lg font-bold hover:text-primaryColor transition duration-300 linear ${
+                                router.pathname === '/products' ? 'text-primaryColor' : 'text-black'
+                            }`}
+                            href="/products"
+                        >
+                            Our Products
+                        </a>
+                    </li>
+                    <li>
+                        <p
+                            className="relative pr-14 text-lg font-bold hover:text-primaryColor transition duration-300 linear"
+                            href="/page"
+                            onMouseEnter={() => setIsOpen(true)}
+                            onMouseLeave={() => setIsOpen(false)}
+                        >
+                            Page
+                            <div
+                                className={`${
+                                    isOpen ? 'opacity-100' : 'opacity-0'
+                                } bg-gray-900 transition-opacity duration-300 absolute left-0 mt-2 w-40 rounded-lg`}
+                            >
+                                <ul className="py-4">
+                                    <li>
+                                        <a href="/" className="pl-3 mb-2 text-white hover:text-primaryColor font-thin">
+                                            Shopping Cart
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="/" className="pl-3 text-white hover:text-primaryColor font-thin">
+                                            Checkout
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </p>
+                    </li>
+                    <li>
+                        <a
+                            className={`pr-14 text-lg font-bold hover:text-primaryColor transition duration-300 linear ${
+                                router.pathname === '/about' ? 'text-primaryColor' : 'text-black'
+                            }`}
+                            href="/about"
+                        >
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className={`pr-14 text-lg font-bold hover:text-primaryColor transition duration-300 linear ${
+                                router.pathname === '/contact' ? 'text-primaryColor' : 'text-black'
+                            }`}
+                            href="/contact"
+                        >
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+
+                <div className="h-full flex items-center">
+                    <div className="hidden lg:block">
+                        <Link to="/login">
+                            <button className="text-lg font-bold mr-3 hover:text-primaryColor">Log In</button>
+                        </Link>
+                        <Link to="/signup">
+                            <button className="text-lg text-white font-bold p-3 bg-primaryColor rounded">
+                                Sign UP
+                            </button>
+                        </Link>
+                    </div>
+
+                    <FontAwesomeIcon icon={faBars} className="block lg:hidden text-2xl" />
+                </div>
+            </header>
         </div>
+    );
+};
 
-      </header>
-    </div>
-  )
-}
-
-export default Header
+export default Header;
---
+++ src/components/Home/MainHome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainHome = () => {
+  return (
+    <div className='h-[1000px]'>MainHome</div>
+  )
+}
+
+export default MainHome
